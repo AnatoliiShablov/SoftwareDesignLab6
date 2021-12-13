b665e05d861e2312e41077bfe05a9b124245d268
Anatolii Shablov <anatoliishablov@gmail.com>
2021-12-13 09:07:39 +0300

    catch the error and print it

--- main.cpp
+++ main.cpp
@@ -22,31 +22,36 @@ void readFromFile(std::shared_ptr<TokenizerContext> ctx,
 }
 
 int main(int argc, char* argv[]) {
-  auto tokenizer = std::make_shared<TokenizerContext>();
+  try {
+    auto tokenizer = std::make_shared<TokenizerContext>();
 
-  if (argc == 2 && std::string_view(argv[1]) != "-") {
-    readFromFile(tokenizer, argv[1]);
-  } else {
-    readFromStdin(tokenizer);
+    if (argc == 2 && std::string_view(argv[1]) != "-") {
+      readFromFile(tokenizer, argv[1]);
+    } else {
+      readFromStdin(tokenizer);
+    }
+
+    auto const& fileTokens = tokenizer->tokens();
+
+    auto parser = std::make_shared<ParserVisitor>();
+    std::for_each(fileTokens.begin(), fileTokens.end(),
+                  [&parser](auto token) { token->accept(parser); });
+
+    auto const& parserTokens = parser->tokens();
+
+    auto printer = std::make_shared<PrintVisitor>();
+    std::for_each(parserTokens.begin(), parserTokens.end(),
+                  [&printer](auto token) { token->accept(printer); });
+
+    auto calculator = std::make_shared<CalcVisitor>();
+    std::for_each(parserTokens.begin(), parserTokens.end(),
+                  [&calculator](auto token) { token->accept(calculator); });
+
+    std::cout << std::endl << calculator->result() << std::endl;
+
+  } catch (std::runtime_error const& err) {
+    std::cout << err.what();
+    return -1;
   }
-
-  auto const& fileTokens = tokenizer->tokens();
-
-  auto parser = std::make_shared<ParserVisitor>();
-  std::for_each(fileTokens.begin(), fileTokens.end(),
-                [&parser](auto token) { token->accept(parser); });
-
-  auto const& parserTokens = parser->tokens();
-
-  auto printer = std::make_shared<PrintVisitor>();
-  std::for_each(parserTokens.begin(), parserTokens.end(),
-                [&printer](auto token) { token->accept(printer); });
-
-  auto calculator = std::make_shared<CalcVisitor>();
-  std::for_each(parserTokens.begin(), parserTokens.end(),
-                [&calculator](auto token) { token->accept(calculator); });
-
-  std::cout << std::endl << calculator->result() << std::endl;
-
   return 0;
 }
